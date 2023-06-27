70609eef10e46d8f1a72a2eb71284c38cb77c760
diego <diegoolliveira1999@gmail.com>
2023-06-27 18:55:40 -0300

    correção do card

--- cardapioDBV/src/componets/card/card.tsx
+++ cardapioDBV/src/componets/card/card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 export function Card({price, image, title, type} : CardProps){
     return(
         <div className="card">
-            <img/>
+            <img src="https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?resize=600%2C338&ssl=1"/>
             <h2>{title}</h2>
             <p><b>Valor:</b>{price}</p>
             <h3>{type}</h3>
